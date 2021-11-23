010ce1ca81722928470a115484776bc2586b8402
peng-u-0807 <penguin0807@snu.ac.kr>
2021-11-23 12:32:47 +0900

    Install pods-1
    - DS_Store만 추가적으로 gitignore에 추가하였습니다. 이외는 이전의 'Install pods' PR과 같습니다.

---
+++ Careerly/Careerly/Controller/SearchTabViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchTabViewController.swift
+//  Careerly
+//
+//  Created by 최유림 on 2021/11/23.
+//
+
+import UIKit
+
+class SearchTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
