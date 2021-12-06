6153c48c2a3a87d8ad1d25877d8791ded4c17e65
KWSMooBang <40kimwoosung@gmail.com>
2021-12-06 20:23:13 +0900

    add PostContentCell

--- Facebook/Facebook/Controller/PostViewController.swift
+++ Facebook/Facebook/Controller/PostViewController.swift
@@ -78,3 +78,4 @@ class PostViewController<View: PostView>: UIViewController {
     */
 
 }
+
---
+++ Facebook/Facebook/View/PostContentTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostContentTableViewCell.swift
+//  Facebook
+//
+//  Created by 김우성 on 2021/12/06.
+//
+
+import UIKit
+
+class PostContentTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var contentImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var commentButton: UIButton!
+    @IBOutlet weak var likeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
