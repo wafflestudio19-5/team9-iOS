adfbba9e74470a0cc4393bd4ce5267d0fd34a04c
shp7724 <shp7724@naver.com>
2021-12-17 03:00:58 +0900

    주석 추가

--- Facebook/Facebook/Controller/NewsfeedTabViewController.swift
+++ Facebook/Facebook/Controller/NewsfeedTabViewController.swift
@@ -23,12 +23,14 @@ class NewsfeedTabViewController: BaseTabViewController<NewsfeedTabView> {
     }
     
     func bind() {
+        /// `viewModel.dataList`와 `tableView`의 dataSource를 바인딩합니다.
         viewModel.dataList
             .bind(to: tableView.rx.items(cellIdentifier: "PostCell", cellType: PostTableViewCell.self)) { row, post, cell in
                 cell.configureLabels(with: post)
             }
             .disposed(by: disposeBag)
         
+        /// `isLoading` 값이 바뀔 때마다 하단 스피너를 토글합니다.
         viewModel.isLoading
             .asDriver()
             .drive(onNext: { [weak self] isLoading in
@@ -40,12 +42,14 @@ class NewsfeedTabViewController: BaseTabViewController<NewsfeedTabView> {
             })
             .disposed(by: disposeBag)
         
+        /// 새로고침 제스쳐가 인식될 때마다 `refresh` 함수를 실행합니다.
         tabView.refreshControl.rx.controlEvent(.valueChanged)
             .subscribe(onNext: { [weak self] in
                 self?.viewModel.refresh()
             })
             .disposed(by: disposeBag)
         
+        /// 새로고침이 완료될 때마다 `refreshControl`의 애니메이션을 중단시킵니다.
         viewModel.refreshComplete
             .asDriver()
             .drive(onNext : { [weak self] refreshComplete in
@@ -55,6 +59,7 @@ class NewsfeedTabViewController: BaseTabViewController<NewsfeedTabView> {
             })
             .disposed(by: disposeBag)
         
+        /// 테이블 맨 아래까지 스크롤할 때마다 `loadMore` 함수를 실행합니다.
         tableView.rx.didScroll.subscribe { [weak self] _ in
             guard let self = self else { return }
             let offSetY = self.tableView.contentOffset.y
