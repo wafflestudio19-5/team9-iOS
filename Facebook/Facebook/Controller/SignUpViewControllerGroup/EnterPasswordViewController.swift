d4ed90df10843c1c7afe110963696a47edc66053
peng-u-0807-macbook <penguin0807@snu.ac.kr>
2021-12-19 19:59:01 +0900

    Add Password Validation
    - 페이스북 앱에서는 password 입력하는 textField가 secure하게 설정되어 있지 않더군요..클론 앱에서는 secureTextField로 설정하였습니다
    - 비밀번호 칸이 비어있을 때, 1자 이상 6자 미만일 때, 6자 이상이지만 부적절한 문자가 표현되어 있을 때, 6자 이상이고 적절한 문자로만 구성되어 있을 때(총 4가지) 상황에 따른 alertLabelMessage 설정하였습니다

--- Facebook/Facebook/Controller/SignUpViewControllerGroup/EnterPasswordViewController.swift
+++ Facebook/Facebook/Controller/SignUpViewControllerGroup/EnterPasswordViewController.swift
@@ -7,8 +7,29 @@
 
 import RxSwift
 import RxGesture
+import RxCocoa
+import Darwin
 
 class EnterPasswordViewController: BaseSignUpViewController<EnterPasswordView> {
+    
+    private enum PasswordValidation {
+        case valid
+        case lessThanSixLetters
+        case emptyLetters
+        case invalidLetters
+        
+        func message() -> String {
+            switch self {
+            case .valid: return ""
+            case .lessThanSixLetters: return "비밀번호는 6자 이상이어야 합니다. 다시 입력해 주세요."
+            case .emptyLetters, .invalidLetters: return "숫자, 영문, 특수기호(!, & 등)를 조합한 여섯 자리 이상의 비밀번호를 입력하세요."
+            }
+        }
+    }
+    
+    private let password = BehaviorRelay<String>(value: "")
+    
+    private var isValidPassword: PasswordValidation?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +38,17 @@ class EnterPasswordViewController: BaseSignUpViewController<EnterPasswordView> {
     }
     
     private func bindView() {
+        
+        customView.passwordTextField.rx.text.orEmpty
+            .bind(to: password)
+            .disposed(by: disposeBag)
+        
+        customView.passwordTextField.rx.text.orEmpty
+            .map { [weak self] password -> PasswordValidation in
+                return self?.isValid(password: password) ?? .emptyLetters }
+            .bind { [weak self] isValid in
+                self?.isValidPassword = isValid
+            }.disposed(by: disposeBag)
         
         view.rx.tapGesture(configuration: { _, delegate in
             delegate.touchReceptionPolicy = .custom { _, shouldReceive in
@@ -29,10 +61,36 @@ class EnterPasswordViewController: BaseSignUpViewController<EnterPasswordView> {
             }
         }.disposed(by: disposeBag)
         
-        customView.nextButton.rx.tap.bind {
-            let rootTabBarController = RootTabBarController()
-            guard let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate else { return }
-            sceneDelegate.changeRootViewController(rootTabBarController)
+        customView.nextButton.rx.tap
+            .asDriver()
+            .drive { [weak self] _ in
+                guard let self = self else { return }
+                guard let isValidPassword = self.isValidPassword else { return }
+                
+                self.customView.setAlertLabelText(as: isValidPassword.message())
+                
+                if isValidPassword == .valid {
+                    // TODO: User 등록
+                    let rootTabBarController = RootTabBarController()
+                    guard let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate else { return }
+                    sceneDelegate.changeRootViewController(rootTabBarController)
+                }
         }.disposed(by: disposeBag)
+    }
+}
+
+extension EnterPasswordViewController {
+    private func isValid(password: String) -> PasswordValidation {
+        // "숫자, 영문, 특수기호(!, & 등)를 포함한 여섯 자리 이상"
+        if password.isEmpty { return PasswordValidation.emptyLetters }
+        else if password.count < 6 { return PasswordValidation.lessThanSixLetters }
+        else {
+            let pattern = "^[A-Za-z0-9!&]*$"
+            let regex = try? NSRegularExpression(pattern: pattern)
+            if let _ = regex?.firstMatch(in: password, range: NSRange(location: 0, length: password.count)) {
+                return PasswordValidation.valid
+            }
+            return PasswordValidation.invalidLetters
+        }
     }
 }
--- Facebook/Facebook/View/SignUpViewGroup/EnterPasswordView.swift
+++ Facebook/Facebook/View/SignUpViewGroup/EnterPasswordView.swift
@@ -11,6 +11,9 @@ class EnterPasswordView: UIView {
 
     let passwordTextField = FacebookTextField(placeholderText: "새로운 비밀번호")
     
+    private let verticalStackWithAlertLabel = UIStackView()
+    let alertLabel = LabelWithPadding(padding: UIEdgeInsets(top: 10.0, left: 7.0, bottom: 10.0, right: 7.0))
+    
     let baseSignUpView = BaseSignUpView(title: "비밀번호 선택", instruction: "다른 사람이 추측할 수 없는 6자 이상의 고유한 비밀번호를 만드세요.")
     
     let nextButton = RectangularSlimButton(title: "가입하기", titleColor: .white, backgroundColor: FacebookColor.blue.color())
@@ -19,6 +22,7 @@ class EnterPasswordView: UIView {
         super.init(frame: frame)
         
         self.backgroundColor = .white
+        setStyleForView()
         setLayoutForView()
     }
     
@@ -26,14 +30,40 @@ class EnterPasswordView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func setAlertLabelText(as text: String) {
+        if text != "" {
+            verticalStackWithAlertLabel.addArrangedSubview(alertLabel)
+            alertLabel.text = text
+            return
+        }
+        verticalStackWithAlertLabel.removeArrangedSubview(alertLabel)
+        alertLabel.removeFromSuperview()
+    }
+    
+    private func setStyleForView() {
+        passwordTextField.isSecureTextEntry = true
+        
+        verticalStackWithAlertLabel.axis = .vertical
+        verticalStackWithAlertLabel.contentMode = .center
+        verticalStackWithAlertLabel.spacing = 14.0
+        
+        alertLabel.font = .systemFont(ofSize: 12.0)
+        alertLabel.textColor = .white
+        alertLabel.backgroundColor = .red
+        alertLabel.numberOfLines = 0
+        alertLabel.lineBreakStrategy = .pushOut
+    }
+    
     private func setLayoutForView() {
 
         self.addSubview(baseSignUpView)
-        self.addSubview(passwordTextField)
+        self.addSubview(verticalStackWithAlertLabel)
         self.addSubview(nextButton)
         
+        verticalStackWithAlertLabel.addArrangedSubview(passwordTextField)
+        
         baseSignUpView.translatesAutoresizingMaskIntoConstraints = false
-        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        verticalStackWithAlertLabel.translatesAutoresizingMaskIntoConstraints = false
         nextButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
@@ -42,11 +72,11 @@ class EnterPasswordView: UIView {
             baseSignUpView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
             baseSignUpView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
             
-            passwordTextField.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 18.0),
-            passwordTextField.topAnchor.constraint(equalTo: baseSignUpView.bottomAnchor, constant: 24.0),
-            passwordTextField.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -18.0),
+            verticalStackWithAlertLabel.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 18.0),
+            verticalStackWithAlertLabel.topAnchor.constraint(equalTo: baseSignUpView.bottomAnchor, constant: 24.0),
+            verticalStackWithAlertLabel.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -18.0),
 
-            nextButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 16.0),
+            nextButton.topAnchor.constraint(equalTo: verticalStackWithAlertLabel.bottomAnchor, constant: 16.0),
             nextButton.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 72.0),
             nextButton.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -72.0),
         ])
