678a5a113ed14aac9e812b8adb964e8874427def
peng-u-0807-macbook <penguin0807@snu.ac.kr>
2021-12-19 16:29:12 +0900

    Add Email Validation
    - 페이스북 앱에서는 textField가 빈칸만 아니면 되지만 클론 앱에서는 정규식을 이용한 간단한 validation을 추가하였습니다
    - "영어 대문자, 소문자 혹은 숫자 1개 이상"@"영어 소문자 1개 이상"."영어 소문자 2개 이상" 일 경우 valid하다고 판단 후 다음 페이지로 이동
    - 예시: (valid) abc@a.kr 혹은 8312@abc.com
               (invalid) abc@a. 혹은 a90@naver.cOM

--- Facebook/Facebook/View/SignUpViewGroup/EnterEmailView.swift
+++ Facebook/Facebook/View/SignUpViewGroup/EnterEmailView.swift
@@ -11,6 +11,10 @@ class EnterEmailView: UIView {
 
     let emailTextField = FacebookTextField(placeholderText: "이메일 주소")
     
+    private let verticalStackWithAlertLabel = UIStackView()
+    
+    let alertLabel = LabelWithPadding(padding: UIEdgeInsets(top: 10.0, left: 7.0, bottom: 10.0, right: 7.0))
+    
     let baseSignUpView = BaseSignUpView(title: "이메일 주소를 입력하세요", instruction: "회원님에게 연락할 수 있는 이메일 주소를 입력하세요. 나중에 프로필에서 이 정보를 숨길 수 있습니다.")
     
     let nextButton = RectangularSlimButton(title: "다음", titleColor: .white, backgroundColor: FacebookColor.blue.color())
@@ -19,6 +23,7 @@ class EnterEmailView: UIView {
         super.init(frame: frame)
         
         self.backgroundColor = .white
+        setStyleForView()
         setLayoutForView()
     }
     
@@ -26,14 +31,38 @@ class EnterEmailView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func setAlertLabelText(as text: String) {
+        if text != "" {
+            verticalStackWithAlertLabel.addArrangedSubview(alertLabel)
+            alertLabel.text = text
+            return
+        }
+        verticalStackWithAlertLabel.removeArrangedSubview(alertLabel)
+        alertLabel.removeFromSuperview()
+    }
+    
+    private func setStyleForView() {
+        emailTextField.autocapitalizationType = .none
+        
+        verticalStackWithAlertLabel.axis = .vertical
+        verticalStackWithAlertLabel.contentMode = .center
+        verticalStackWithAlertLabel.spacing = 14.0
+        
+        alertLabel.font = .systemFont(ofSize: 12.0)
+        alertLabel.textColor = .white
+        alertLabel.backgroundColor = .red
+    }
+    
     private func setLayoutForView() {
 
         self.addSubview(baseSignUpView)
-        self.addSubview(emailTextField)
+        self.addSubview(verticalStackWithAlertLabel)
         self.addSubview(nextButton)
         
+        verticalStackWithAlertLabel.addArrangedSubview(emailTextField)
+        
         baseSignUpView.translatesAutoresizingMaskIntoConstraints = false
-        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        verticalStackWithAlertLabel.translatesAutoresizingMaskIntoConstraints = false
         nextButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
@@ -42,14 +71,13 @@ class EnterEmailView: UIView {
             baseSignUpView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
             baseSignUpView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
             
-            emailTextField.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 18.0),
-            emailTextField.topAnchor.constraint(equalTo: baseSignUpView.bottomAnchor, constant: 24.0),
-            emailTextField.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -18.0),
+            verticalStackWithAlertLabel.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 18.0),
+            verticalStackWithAlertLabel.topAnchor.constraint(equalTo: baseSignUpView.bottomAnchor, constant: 24.0),
+            verticalStackWithAlertLabel.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -18.0),
 
-            nextButton.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 16.0),
+            nextButton.topAnchor.constraint(equalTo: verticalStackWithAlertLabel.bottomAnchor, constant: 16.0),
             nextButton.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 72.0),
             nextButton.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -72.0),
         ])
     }
-
 }
