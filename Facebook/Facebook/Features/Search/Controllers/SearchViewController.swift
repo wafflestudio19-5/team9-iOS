647404a26035171698922cc675d4faf1ee5d8718
peng-u-0807-macbook <penguin0807@snu.ac.kr>
2022-01-04 15:13:27 +0900

    Chore: 불필요한 코드 정리

--- Facebook/Facebook/Features/Search/Controllers/SearchViewController.swift
+++ Facebook/Facebook/Features/Search/Controllers/SearchViewController.swift
@@ -33,15 +33,5 @@ class SearchViewController: UIViewController {
         searchBar.placeholder = "Facebook 검색"
         self.navigationItem.titleView = searchBar
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
