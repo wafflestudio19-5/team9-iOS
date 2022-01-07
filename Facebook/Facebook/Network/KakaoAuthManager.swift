b238727d3f26d4deb7825946a0ca38d1be0ce53e
peng-u-0807-macbook <penguin0807@snu.ac.kr>
2022-01-07 14:34:48 +0900

    Add KakaoAuthManager

--- Facebook/Facebook/Network/KakaoAuthManager.swift
+++ Facebook/Facebook/Network/KakaoAuthManager.swift
@@ -9,3 +9,40 @@ import RxSwift
 import KakaoSDKUser
 import RxKakaoSDKUser
 import KakaoSDKAuth
+
+class KakaoAuthManager {
+    
+    private let disposeBag = DisposeBag()
+    
+    static let shared = KakaoAuthManager()
+    private init() { }
+    
+    func requestKakaoLogin() -> Observable<Bool> {
+        return Observable<Bool>.create { result in
+            if UserApi.isKakaoTalkLoginAvailable() {
+                UserApi.shared.rx.loginWithKakaoTalk()
+                    .subscribe(onNext: { response in
+                        result.onNext(true)
+                    }, onError: { _ in
+                        result.onNext(false)
+                    }).disposed(by: self.disposeBag)
+            } else {
+                UserApi.shared.rx.loginWithKakaoAccount()
+                    .subscribe(onNext: { _ in
+                        result.onNext(true)
+                    }, onError: { _ in
+                        result.onNext(false)
+                    }).disposed(by: self.disposeBag)
+            }
+            return Disposables.create()
+        }
+    }
+    
+    func connectKakaoAccount(accessToken: String, userToken: String) {
+        // 카카오 access token과 유저의 JWT 토큰으로 서버에 "카카오 계정 연결" 요청
+    }
+    
+    func loginKakaoAccount(accessToken: String) {
+        // 카카오 access token으로 서버에 "카카오 계정으로 로그인" 요청
+    }
+}
