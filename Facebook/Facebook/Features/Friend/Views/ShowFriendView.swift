6b9999b10a2e2b6abdd23140855ea9eeba1e128e
KIMWOOSUNG <40kimwoosung@gmail.co>
2022-01-14 14:25:43 +0900

    ShowFriend 뷰의 Search TableHeaderView 추가

--- Facebook/Facebook/Features/Friend/Views/ShowFriendView.swift
+++ Facebook/Facebook/Features/Friend/Views/ShowFriendView.swift
@@ -10,6 +10,13 @@ import UIKit
 class ShowFriendView: UIView {
     
     let showFriendTableView = UITableView()
+    let headerView = UIView()
+    let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "친구 검색"
+        
+        return searchBar
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -26,9 +33,16 @@ class ShowFriendView: UIView {
         showFriendTableView.snp.makeConstraints { make in
             make.edges.equalTo(self.safeAreaLayoutGuide)
         }
+        
+        headerView.addSubview(searchBar)
+        searchBar.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview().inset(10)
+            make.leading.trailing.equalToSuperview().inset(15)
+        }
     }
     
     private func configureTableView() {
+        showFriendTableView.tableHeaderView = headerView
         showFriendTableView.register(FriendCell.self, forCellReuseIdentifier: FriendCell.reuseIdentifier)
         showFriendTableView.allowsSelection = false
         showFriendTableView.delaysContentTouches = false
