184d3e8ffd03d7115ebf0bb192e8faf0620aa844
peng-u-0807-macbook <penguin0807@snu.ac.kr>
2022-01-17 21:18:23 +0900

    Add Notification Model

--- Facebook/Facebook/Features/Notification/Controllers/NotificationTabViewController.swift
+++ Facebook/Facebook/Features/Notification/Controllers/NotificationTabViewController.swift
@@ -6,12 +6,25 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxDataSources
 
-class NotificationTabViewController: BaseTabViewController<NotificationTabView> {
+class NotificationTabViewController: BaseTabViewController<NotificationTabView>, UIScrollViewDelegate {
 
+    var tableView: UITableView {
+        tabView.notificationTableView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: tabView.largeTitleLabel)
+        
+        bind()
+    }
+    
+    private func bind() {
+        tableView.rx.setDelegate(self).disposed(by: disposeBag)
     }
     
 }
