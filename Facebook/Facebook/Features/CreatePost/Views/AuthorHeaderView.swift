354ce67b25e3856f61f63ee4d53d1b350a4ff043
shinhong_park <shp7724@snu.ac.kr>
2022-01-22 14:31:13 +0900

    Scope 리팩토링 및 공개범위 뉴스피드 표시

--- Facebook/Facebook/Features/CreatePost/Views/AuthorHeaderView.swift
+++ Facebook/Facebook/Features/CreatePost/Views/AuthorHeaderView.swift
@@ -8,19 +8,13 @@
 import UIKit
 import RxSwift
 
-enum Scope: Int {
-    case all = 3
-    case friends = 2
-    case secret = 1
-}
-
 class CreateHeaderView: UIView {
     
     /// 피드 상단, 게시자의 프로필 이미지와 공개 범위 버튼이 표시되는 뷰
     
     var selectedScope: Scope = .all {
         didSet {
-            self.configureScopeButton()
+            scopeButton.configure(with: selectedScope)
         }
     }
     var disposeBag = DisposeBag()
@@ -51,7 +45,7 @@ class CreateHeaderView: UIView {
         self.addSubview(labelStack)
         labelStack.snp.makeConstraints { make in
             make.centerY.equalToSuperview()
-            make.leading.equalTo(profileImageView.snp.trailing).offset(5)
+            make.leading.equalTo(profileImageView.snp.trailing).offset(8)
             make.trailing.equalTo(0).offset(CGFloat.standardTrailingMargin)
         }
         
@@ -65,42 +59,24 @@ class CreateHeaderView: UIView {
     func configure() {
         profileImageView.setImage(from: StateManager.of.user.profile.profile_image)
         authorNameLabel.text = StateManager.of.user.profile.username
-        configureScopeButton()
         scopeButton.menu = scopeMenu
         scopeButton.showsMenuAsPrimaryAction = true
-    }
-    
-    func configureScopeButton() {
-        switch selectedScope {
-        case .all:
-            scopeButton.configurePublic()
-        case .friends:
-            scopeButton.configureFriendsOnly()
-        case .secret:
-            scopeButton.configurePrivate()
-        }
+        scopeButton.configure(with: .all)
     }
     
     let profileImageView = ProfileImageView()
-    let authorNameLabel: InfoLabel = InfoLabel(color: .label, size: 15, weight: .medium)
+    let authorNameLabel: InfoLabel = InfoLabel(color: .label, size: 15, weight: .semibold)
     let scopeButton = ScopeButton(size: 12, weight: .regular)
     
     private var scopeMenues: [UIAction] {
-        return [
-            UIAction(title: "전체 공개", image: UIImage(systemName: "globe.asia.australia"), handler: { _ in
-                self.selectedScope = .all
-            }),
-            UIAction(title: "친구만", image: UIImage(systemName: "person.2"), handler: { _ in
-                self.selectedScope = .friends
-            }),
-            UIAction(title: "나만 보기", image: UIImage(systemName: "lock"), handler: { _ in
-                self.selectedScope = .secret
+        return Scope.allCases.map { scope in
+            return UIAction(title: scope.text, image: scope.getImage(fill: false), handler: { _ in
+                self.selectedScope = scope
             })
-        ]
+        }
     }
     
     private var scopeMenu: UIMenu {
         return UIMenu(title: "", image: nil, identifier: nil, options: [], children: scopeMenues)
     }
 }
-
--- Facebook/Facebook/Features/CreatePost/Views/ScopeButton.swift
+++ Facebook/Facebook/Features/CreatePost/Views/ScopeButton.swift
@@ -13,26 +13,13 @@ class ScopeButton: InfoButton {
         return UIImage(systemName: name, withConfiguration: UIImage.SymbolConfiguration(pointSize: 10, weight: .regular))!
     }
     
-    private func baseConfigure(symbolName: String, text: String) {
+    func configure(with scope: Scope) {
         configuration?.imagePadding = 5
-        configuration?.image = getImage(name: symbolName)
+        configuration?.image = scope.getImage(fill: true)
         self.layer.cornerRadius = 5
         self.clipsToBounds = true
         self.layer.borderWidth = 1
         self.layer.borderColor = UIColor.grayscales.border.cgColor
-        self.text = text
+        self.text = scope.text
     }
-    
-    func configurePublic() {
-        baseConfigure(symbolName: "globe.asia.australia.fill", text: "전체 공개")
-    }
-    
-    func configureFriendsOnly() {
-        baseConfigure(symbolName: "person.2.fill", text: "친구만")
-    }
-    
-    func configurePrivate() {
-        baseConfigure(symbolName: "lock.fill", text: "나만 보기")
-    }
-
 }
