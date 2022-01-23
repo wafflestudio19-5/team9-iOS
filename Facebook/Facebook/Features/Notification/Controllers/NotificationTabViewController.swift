709b1fe84fc6b3ea83934b9613b10ddd52c27009
peng-u-0807-macbook <penguin0807@snu.ac.kr>
2022-01-23 15:10:16 +0900

    Add Dark background when cell tapped

--- Facebook/Facebook/Features/Notification/Controllers/NotificationTabViewController.swift
+++ Facebook/Facebook/Features/Notification/Controllers/NotificationTabViewController.swift
@@ -15,6 +15,8 @@ class NotificationTabViewController: BaseTabViewController<NotificationTabView>,
         tabView.notificationTableView
     }
     
+    private let isShowingBottomSheet: BehaviorRelay<Bool> = BehaviorRelay(value: false)
+    
     let viewModel = PaginationViewModel<Notification>(endpoint: .notification())
     
     override func viewDidLoad() {
@@ -29,17 +31,36 @@ class NotificationTabViewController: BaseTabViewController<NotificationTabView>,
         
         viewModel.dataList
             .observe(on: MainScheduler.instance)
-            .bind(to: tableView.rx.items(cellIdentifier: NotificationTableViewCell.reuseIdentifier, cellType: NotificationTableViewCell.self)) { index, notification, cell in
-                print(notification)
-                cell.configure(with: notification)
+            .bind(to: tableView.rx.items(cellIdentifier: NotificationCell.reuseIdentifier, cellType: NotificationCell.self)) { [weak self] index, notification, cell in
+                self?.configure(cell: cell, with: notification)
             }.disposed(by: disposeBag)
 
         StateManager.of.notification.bind(with: viewModel.dataList).disposed(by: disposeBag)
+        
+        tabView.bottomSheetView.rx.tapGesture().bind { [weak self] _ in
+            self?.isShowingBottomSheet.accept(false)
+        }.disposed(by: disposeBag)
+        
+        isShowingBottomSheet
+            .observe(on: MainScheduler.instance)
+            .bind { [weak self] isShowing in
+            isShowing ? self?.tabView.showBottomSheetView() : self?.tabView.dismissBottomSheetView()
+        }.disposed(by: disposeBag)
 
         // cell을 탭할 경우 상세 페이지로 이동
 //        tableView.rx.modelSelected(Notification.self)
 //            .bind { [weak self] notification in
 //
 //            }.disposed(by: disposeBag)
+    }
+}
+
+extension NotificationTabViewController {
+    private func configure(cell: NotificationCell, with notification: Notification) {
+        cell.configure(with: notification)
+        
+        cell.detailButton.rx.tap.bind { [weak self] in
+            self?.isShowingBottomSheet.accept(true)
+        }.disposed(by: disposeBag)
     }
 }
