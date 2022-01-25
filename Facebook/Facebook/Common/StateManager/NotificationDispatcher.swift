f03911dd416b532b16c404573babab9f810cba4a
peng-u-0807 <penguin0807@snu.ac.kr>
2022-01-25 20:58:45 +0900

    Add 알림 확인/삭제 dispatch

--- Facebook/Facebook/Common/StateManager/NotificationDispatcher.swift
+++ Facebook/Facebook/Common/StateManager/NotificationDispatcher.swift
@@ -10,5 +10,15 @@ import RxRelay
 
 class NotificationDispatcher: Dispatcher<Notification> {
     
+    /// 알림을 확인한 경우
+    func dispatch(check notification: Notification) {
+        let notification = notification
+        dispatchedSignals.accept(.init(data: notification, operation: .edit))
+    }
     
+    /// 알림을 삭제한 경우
+    func dispatch(delete notification: Notification) {
+        let notification = notification
+        dispatchedSignals.accept(.init(data: notification, operation: .delete(index: notification.id)))
+    }
 }
