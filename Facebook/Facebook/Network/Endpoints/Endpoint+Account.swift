34cd9e7bcf18943f7c00a41b342f2218eea4a2b7
KIMWOOSUNG <40kimwoosung@gmail.co>
2022-01-25 22:53:09 +0900

    겹치는 코드 제거

--- Facebook/Facebook/Network/Endpoints/Endpoint+Account.swift
+++ Facebook/Facebook/Network/Endpoints/Endpoint+Account.swift
@@ -28,9 +28,6 @@ extension Endpoint {
         return Endpoint(path: "account/signup/", parameters: ["email": newUser.email!, "first_name": newUser.first_name!, "last_name": newUser.last_name!, "birth": newUser.birth!, "gender": newUser.gender!, "password": newUser.password!])
     }
     
-    static func refreshToken(token: String) -> Self {
-        return Endpoint(path: "token/refresh/", parameters: ["token": token])
-
     static func deleteAccount() -> Self {
         return Endpoint(path: "account/delete/")
     }
