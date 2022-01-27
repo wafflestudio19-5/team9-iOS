5f954f8bca52d2021b9e0aa2fc13c6ee204a727f
shinhong_park <shp7724@snu.ac.kr>
2022-01-28 02:06:34 +0900

    댓글 InfoButton 바인딩

--- Facebook/Facebook/Features/PostDetail/Controllers/SubPostsViewController.swift
+++ Facebook/Facebook/Features/PostDetail/Controllers/SubPostsViewController.swift
@@ -110,10 +110,14 @@ class SubPostsViewController: UIViewController {
                 }.disposed(by: cell.refreshingBag)
                 
                 // 댓글 버튼 바인딩
-                cell.postContentView.commentButton.rx.tap.bind { [weak self] _ in
-                    guard let self = self else { return }
-                    self.presentCommentModalVC(to: cell.postContentView.post)
-                }.disposed(by: cell.refreshingBag)
+                let commentButtonTapped = cell.postContentView.commentButton.rx.tap
+                let commentLabelTapped = cell.postContentView.commentCountButton.rx.tap
+                Observable.of(commentButtonTapped, commentLabelTapped)
+                    .merge()
+                    .bind { [weak self] _ in
+                        guard let self = self else { return }
+                        self.presentCommentModalVC(to: cell.postContentView.post)
+                    }.disposed(by: cell.refreshingBag)
                 
                 // 공유 버튼 바인딩
                 cell.postContentView.shareButton.rx.tap.bind { [weak self] _ in
